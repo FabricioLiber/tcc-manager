6477634adbe2a4031f5475185373bd33aebd4051
kmlporto <kmilaporto1991@gmail.com>
2019-12-07 23:19:52 -0300

    Revert "adiciona gems, scaffold user, e paginacao"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,4 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
-//= require cocoon
-
 //= require_tree .
